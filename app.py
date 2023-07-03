a6f5d6fbba17c5c476cd5fd2e777affabbb4a03b
barrymoana <62818864+barrymoana@users.noreply.github.com>
2023-07-03 15:43:42 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pickle
 import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
